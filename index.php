9813857c5b3f83b0d9411cd2fce6b86c43bd3065
Weston Verhulst <w2visme@gmail.com>
2018-05-02 21:54:29 -0500

    Added login/logout functionality

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <title>Weston's Movies</title>
     <link rel="stylesheet" type="text/css" href="css/main.css"/>
-    
     <!--Materialize CSS: Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
 
@@ -17,20 +16,19 @@
         <div class="navbar-wrapper container">
             <a href="index.html" class="brand-logo">Weston's Movies</a>
             <ul class="right">
-                <li><a href="index.html">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <li><a class="pulse" href="login/login-form.php">Login</a></li>
 
             </ul>
         </div>
     </nav>
     <h5>Welcome to Weston's Movies review site. Checkout all the movies I've made reviews for below! blah blah </h5>
-    <div class="divider"></div>
     <div class="carousel">
-        <a id="wind-river" class="carousel-item hoverable" href="movie.html"><img alt="Wind River" src="https://image.tmdb.org/t/p/w500/pySivdR845Hom4u4T2WNkJxe6Ad.jpg"></a>
-        <a id="thor-ragnarok" class="carousel-item hoverable" href="movie.html"><img alt="Thor Ragnarok" src="https://image.tmdb.org/t/p/w500/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg"></a>
-        <a id="ready-player-one" class="carousel-item hoverable" href="movie.html"><img alt="Ready Player One" src="https://image.tmdb.org/t/p/w500/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg"></a>
-        <a id="Kingsman-a-golden-circle" class="carousel-item hoverable" href="movie.html"><img alt="Kingsman: A Golden Circle" src="https://image.tmdb.org/t/p/w500/34xBL6BXNYFqtHO9zhcgoakS4aP.jpg"></a>
-        <a id="get-out" class="carousel-item hoverable" href="movie.html"><img alt="Get Out" src="https://image.tmdb.org/t/p/w500/1SwAVYpuLj8KsHxllTF8Dt9dSSX.jpg"></a>
+        <a id="wind-river" class="carousel-item hoverable" href="movie.php"><img alt="Wind River" src="https://image.tmdb.org/t/p/w500/pySivdR845Hom4u4T2WNkJxe6Ad.jpg"></a>
+        <a id="thor-ragnarok" class="carousel-item hoverable" href="movie.php"><img alt="Thor Ragnarok" src="https://image.tmdb.org/t/p/w500/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg"></a>
+        <a id="ready-player-one" class="carousel-item hoverable" href="movie.php"><img alt="Ready Player One" src="https://image.tmdb.org/t/p/w500/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg"></a>
+        <a id="Kingsman-a-golden-circle" class="carousel-item hoverable" href="movie.php"><img alt="Kingsman: A Golden Circle" src="https://image.tmdb.org/t/p/w500/34xBL6BXNYFqtHO9zhcgoakS4aP.jpg"></a>
+        <a id="get-out" class="carousel-item hoverable" href="movie.php"><img alt="Get Out" src="https://image.tmdb.org/t/p/w500/1SwAVYpuLj8KsHxllTF8Dt9dSSX.jpg"></a>
   </div>
     <footer class="page-footer black">
         <div class="footer-copyright">
--- login/loginValidate.php
+++ login/loginValidate.php
@@ -1,12 +1,26 @@
 <?php
+    // if(!session_start()){
+    //     header("Location: error.php");
+    //     exit;
+    // }
+
+    // $loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
+    // if($loggedIn){
+    //     header("Location: ../index.php");
+    // }
+    session_start();
+
     $adminUsername = "test";
     $adminPassword="pass";
-
+    $username = '';
+    $password = '';
     if(isset($_POST['login'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
         if($username == $adminUsername and $password == $adminPassword){
+            $_SESSION['loggedin'] = $username;
             header("location: test.php");
+            exit;
         }else{
 
             echo("Email or password is invalid.<br> Please click <a href='login-form.php'>here to try again</a>.");
